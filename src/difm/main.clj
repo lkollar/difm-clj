36e3c856015fd1246bfffc4c2d17401158f7adbc
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-02-01 16:01:49 +0000

    Fix fetching new tracks
    Avoid doing this by returning a lazy-seq as it makes it really
    complicated to work with it. Instead, simply just take the seen-tracks
    as an argument and keep checking if we've already seen a track we get in
    the response.

--- src/difm/main.clj
+++ src/difm/main.clj
@@ -13,69 +13,45 @@
                                      (println "Could not read 'config.edn'")
                                      (System/exit 1)))))
 
-;(defn fetch-all-tracks
-;  [mix-show-name username password stop-fn]
-;  (loop [counter 1
-;         all-episodes []]
-;    (let [episodes (audioaddict/get-mixshow-episodes username password mix-show-name counter)]
-;      (if (or (empty? episodes) (stop-fn episodes))
-;        all-episodes
-;        (recur (inc counter) (concat all-episodes episodes))))))
 
 (defn fetch-all-tracks
-  [mix-show-name username password]
-  (letfn [(fetch-page [page]
-            (let [episodes (audioaddict/get-mixshow-episodes username password mix-show-name page)]
-              (if (empty? episodes)
-                nil
-                (lazy-seq (concat episodes (fetch-page (inc page)))))))]
-    (fetch-page 1)))
-
-(defn fetch-all-tracks
-  ([username password mix-show-name]
-   (fetch-all-tracks username password mix-show-name 1))
-  ([username password mix-show-name page]
-   (let [episodes (audioaddict/get-mixshow-episodes username password mix-show-name page)]
-     (if (empty? episodes)
-       nil
-       (lazy-seq (concat episodes (fetch-all-tracks username password mix-show-name (inc page))))))))
+  [mix-show-name username password seen-tracks]
+  (letfn [(get-track-id [track-response] (:id (first (:tracks track-response))))
+          (contains-seen-track [episodes] (some (set (map get-track-id episodes)) seen-tracks))]
+    (loop [counter 1
+           all-episodes []]
+      (let [episodes (audioaddict/get-mixshow-episodes username password mix-show-name counter)]
+        (do
+          (let [track-ids (map get-track-id episodes)]
+            (println "Got track ids: " track-ids)
+            (println "Seen track ids: " seen-tracks)))
+        (if (or (empty? episodes) (contains-seen-track episodes))
+          all-episodes
+          (recur (inc counter) (concat all-episodes episodes)))))))
 
 
-; TODO change fetch-all-tracks to take a seen-tracks, and stop querying when it sees
-; any songs from that list. or take a single seen-track
 (defn -main
   []
   (let [config (load-config-or-die)
-        seen-tracks (io/load-seen-tracks "saved_track_ids.txt")]))
+        seen-tracks (io/load-seen-tracks "saved_track_ids.txt")
+        new-tracks (fetch-all-tracks "in-the-mix" (:user config) (:password config) seen-tracks)]
+    (println (pr-str "Found new tracks: " new-tracks))))
 
 (comment
+  (-main)
   (io/parse-config-file "config.edn")
   (try
     (io/parse-config-file "config1.edn")
     (catch Exception e (println "Failed to parse configuration")))
 
-  ;(io/save-seen-tracks "save_tracks.txt" [1 2 3])
   ;(io/load-seen-tracks "saved_track_ids.txt")
   (io/load-seen-tracks "nosuchfile")
 
   (def config (io/parse-config-file "config.edn"))
-  ;(take 2 (fetch-all-tracks (:user config) (:password config) "in-the-mix"))
-  (take-while #(not= (:id %) 186007) (fetch-all-tracks (:user config) (:password config) "in-the-mix"))
-
-  (defn contains-id?
-    [coll id]
-    (some true? (for [x coll
-                      :let [_id (:id x)]]
-                  (= _id id))))
-
-  (contains-id? fos 186006)
-
-  ;(fetch-all-tracks "in-the-mix" (:user config) (:password config) #(contains-id? % 186006))
-
 
   (spit "in-the-mix-shows.edn" (pr-str in-the-mix-shows))
+  (def in-the-mix-shows (clojure.edn/read-string (slurp "in-the-mix-shows.edn")))
   in-the-mix-shows
-  (def fos (take 10 in-the-mix-shows))
 
 
   :rcf)
