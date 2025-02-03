409b253f75aa20ec69ec2d82fd372ba7ecad49df
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-02-03 22:16:57 +0000

    Silence jaudiotagger logs
    These are super verbose and spam stdout.

--- src/difm/main.clj
+++ src/difm/main.clj
@@ -3,7 +3,10 @@
             [difm.audioaddict :as audioaddict]
             [difm.id3tagger :as id3tagger]
             [difm.io :as io])
-  (:import (java.io FileNotFoundException)))
+  (:import (java.io FileNotFoundException)
+           (java.util.logging Level Logger))
+  (:gen-class))
+
 
 ; TODO Work out CWD and always try to read config files from there
 ; TODO validate config schema
@@ -75,13 +78,16 @@
       (update-id3-tags track-info file-name))))
 
 
+; TODO update save_track_ids.txt
 (defn -main
-  []
+  [& args]
   (let [config (load-config-or-die)
         username (:user config)
         password (:password config)
         seen-tracks (io/load-seen-tracks "saved_track_ids.txt")
         new-tracks (fetch-all-tracks "in-the-mix" (:user config) (:password config) seen-tracks)]
     (doseq [track new-tracks]
+      ; Silence jaudiotagger spammy logs
+      (.setLevel (Logger/getLogger "org.jaudiotagger") Level/OFF)
       (println (str "Processing track " (:slug track)))
       (save-track username password track))))
