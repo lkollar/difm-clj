727eb83307067fb9c0c5bd81bedc78d76f8bc763
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-02-03 22:18:52 +0000

    Update saved_track_ids.txt with new tracks

--- src/difm/main.clj
+++ src/difm/main.clj
@@ -7,6 +7,7 @@
            (java.util.logging Level Logger))
   (:gen-class))
 
+(def ^:private seen-tracks-filename "saved_track_ids.txt")
 
 ; TODO Work out CWD and always try to read config files from there
 ; TODO validate config schema
@@ -78,16 +79,21 @@
       (update-id3-tags track-info file-name))))
 
 
-; TODO update save_track_ids.txt
 (defn -main
   [& args]
   (let [config (load-config-or-die)
         username (:user config)
         password (:password config)
-        seen-tracks (io/load-seen-tracks "saved_track_ids.txt")
-        new-tracks (fetch-all-tracks "in-the-mix" (:user config) (:password config) seen-tracks)]
+        seen-tracks (io/load-seen-tracks seen-tracks-filename)
+        new-tracks (fetch-all-tracks "in-the-mix" (:user config) (:password config) seen-tracks)
+        new-track-ids (map #(-> %
+                                :tracks
+                                first
+                                :id)
+                           new-tracks)]
     (doseq [track new-tracks]
       ; Silence jaudiotagger spammy logs
       (.setLevel (Logger/getLogger "org.jaudiotagger") Level/OFF)
       (println (str "Processing track " (:slug track)))
-      (save-track username password track))))
+      (save-track username password track)
+      (io/save-seen-tracks seen-tracks-filename (concat seen-tracks new-track-ids)))))
